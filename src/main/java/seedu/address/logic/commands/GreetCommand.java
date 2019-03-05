eb747fb7b0a37f06e40d38c87af5168dd934b037
suriruhani <suriruhani@gmail.com>
2019-03-05 18:58:08 +0800

    Minor checkstyle fixes

--- src/main/java/seedu/address/logic/commands/GreetCommand.java
+++ src/main/java/seedu/address/logic/commands/GreetCommand.java
@@ -10,9 +10,8 @@ public class GreetCommand extends Command {
 
     public static final String COMMAND_WORD = "greet";
 
-    public static final String MESSAGE_SUCCESS = "Hello there, beautiful human! Hope you have a good experience"
-            + " with our product and a even better day ahead! Come again :)";
-
+    public static final String MESSAGE_SUCCESS = "Hello there, beautiful human! Hope you have a good experience "
+            + "with our product and a even better day ahead! Come again :)";
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {

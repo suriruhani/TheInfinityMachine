290a79473c3eb93b8763e5912e9822378d0ac89c
suriruhani <suriruhani@gmail.com>
2019-03-05 19:19:16 +0800

    Checkstyle fixes

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -20,8 +20,8 @@ import seedu.address.logic.commands.PanicCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.UndoCommand;
-import seedu.address.logic.commands.UnpanicCommand;
 import seedu.address.logic.commands.GreetCommand;
+import seedu.address.logic.commands.UnpanicCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**

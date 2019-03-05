1b3eac2bf9bb7baf211f7463545fdf1da79364ae
DoItTomorrow <E0177248@u.nus.edu>
2019-03-05 19:53:17 +0800

    no message

--- src/main/java/seedu/address/logic/commands/BiblioCommand.java
+++ src/main/java/seedu/address/logic/commands/BiblioCommand.java
@@ -11,7 +11,7 @@ import seedu.address.model.Model;
 public class BiblioCommand extends Command {
     public static final String COMMAND_WORD = "biblio";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Generates a full bibliography of sources using appropriate style.";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Generates a bibliography in appropriate style";
 
     public static final String MESSAGE_SUCCESS = "Bibliography generated";
 

91d60a79b1382cbc833624539ec11c9e5351a126
DoItTomorrow <E0177248@u.nus.edu>
2019-03-05 19:43:57 +0800

    no message

--- src/main/java/seedu/address/logic/commands/BiblioCommand.java
+++ src/main/java/seedu/address/logic/commands/BiblioCommand.java
@@ -1,5 +1,9 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+
 /**
  * Generated a bibliography of the appropriate style.
  */
@@ -12,5 +16,10 @@ public class BiblioCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Bibliography generated";
 
     public BiblioCommand() {}
-    
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+
 }

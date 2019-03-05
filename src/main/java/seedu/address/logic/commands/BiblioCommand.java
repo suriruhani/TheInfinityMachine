9acd1bc279df685c97f96374cb5cc62bce9cf6af
DoItTomorrow <E0177248@u.nus.edu>
2019-03-05 19:29:40 +0800

    Added biblio command

---
+++ src/main/java/seedu/address/logic/commands/BiblioCommand.java
@@ -0,0 +1,17 @@
+package seedu.address.logic.commands;
+
+/**
+ * Generated a bibliography of the appropriate style.
+ */
+
+public class BiblioCommand extends Command {
+    public static final String COMMAND_WORD = "biblio";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Generates a full bibliography of sources using appropriate style.";
+
+    public static final String MESSAGE_SUCCESS = "Bibliography generated";
+
+    public BiblioCommand() {}
+
+    }
+}

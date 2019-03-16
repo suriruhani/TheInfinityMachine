41e12e4081a4dd9eccf2195b13aeb728028a4de3
Fabian <fabianterh@gmail.com>
2019-03-16 23:00:43 +0800

    Update API: rename "address book" references to "source manager"

---
+++ src/main/java/seedu/address/model/source/exceptions/SourceNotFoundException.java
@@ -0,0 +1,6 @@
+package seedu.address.model.source.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified source.
+ */
+public class SourceNotFoundException extends RuntimeException {}

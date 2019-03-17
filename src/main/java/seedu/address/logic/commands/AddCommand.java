7ea32191131c5df5b5bba61e96ccbfd36674eeb1
Cassey Low <casseyl141@gmail.com>
2019-03-17 21:46:41 +0800

    refactored add/delete/edit commands

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -12,7 +12,7 @@ import seedu.address.model.Model;
 import seedu.address.model.source.Source;
 
 /**
- * Adds a person to the address book.
+ * Adds a source to the infinity machine.
  */
 public class AddCommand extends Command {
 

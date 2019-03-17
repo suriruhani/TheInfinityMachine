56d10f019c78b162662ec2dbef41c4170d6aa5f6
Cassey Low <casseyl141@gmail.com>
2019-03-17 21:31:22 +0800

    refactor code in Logic.java and LogicManager.java

--- src/test/java/seedu/address/logic/parser/SourceManagerParserTest.java
+++ src/test/java/seedu/address/logic/parser/SourceManagerParserTest.java
@@ -35,11 +35,11 @@ import seedu.address.testutil.EditPersonDescriptorBuilder;
 import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.PersonUtil;
 
-public class AddressBookParserTest {
+public class SourceManagerParserTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-    private final AddressBookParser parser = new AddressBookParser();
+    private final SourceManagerParser parser = new SourceManagerParser();
 
     @Test
     public void parseCommand_add() throws Exception {

baeff84bdefc390474ce97675be7e31518508e99
Cassey Low <casseyl141@gmail.com>
2019-03-19 01:26:38 +0800

    refactor all test files, removed person model test files, refactor data file

--- src/test/java/seedu/address/ui/BrowserPanelTest.java
+++ src/test/java/seedu/address/ui/BrowserPanelTest.java
@@ -2,7 +2,7 @@ package seedu.address.ui;
 
 import static guitests.guihandles.WebViewUtil.waitUntilBrowserLoaded;
 import static org.junit.Assert.assertEquals;
-import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalSources.ALICE;
 
 import java.net.URL;
 
@@ -11,16 +11,16 @@ import org.junit.Test;
 
 import guitests.guihandles.BrowserPanelHandle;
 import javafx.beans.property.SimpleObjectProperty;
-import seedu.address.model.person.Person;
+import seedu.address.model.source.Source;
 
 public class BrowserPanelTest extends GuiUnitTest {
-    private SimpleObjectProperty<Person> selectedPerson = new SimpleObjectProperty<>();
+    private SimpleObjectProperty<Source> selectedSource = new SimpleObjectProperty<>();
     private BrowserPanel browserPanel;
     private BrowserPanelHandle browserPanelHandle;
 
     @Before
     public void setUp() {
-        guiRobot.interact(() -> browserPanel = new BrowserPanel(selectedPerson));
+        guiRobot.interact(() -> browserPanel = new BrowserPanel(selectedSource));
         uiPartRule.setUiPart(browserPanel);
 
         browserPanelHandle = new BrowserPanelHandle(browserPanel.getRoot());
@@ -31,9 +31,9 @@ public class BrowserPanelTest extends GuiUnitTest {
         // default web page
         assertEquals(BrowserPanel.DEFAULT_PAGE, browserPanelHandle.getLoadedUrl());
 
-        // associated web page of a person
-        guiRobot.interact(() -> selectedPerson.set(ALICE));
-        URL expectedPersonUrl = new URL(BrowserPanel.SEARCH_PAGE_URL + ALICE.getName().fullName.replaceAll(" ", "%20"));
+        // associated web page of a source
+        guiRobot.interact(() -> selectedSource.set(ALICE));
+        URL expectedPersonUrl = new URL(BrowserPanel.SEARCH_PAGE_URL + ALICE.getTitle().title.replaceAll(" ", "%20"));
 
         waitUntilBrowserLoaded(browserPanelHandle);
         assertEquals(expectedPersonUrl, browserPanelHandle.getLoadedUrl());

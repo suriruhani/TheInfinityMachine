ccad0bb75cb55afbb8a414b209e0e2d054450c1c
Cassey Low <casseyl141@gmail.com>
2019-03-19 12:11:04 +0800

    fix travis errors for ui test files

--- src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
+++ src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import guitests.guihandles.ResultDisplayHandle;
 import guitests.guihandles.SourceCardHandle;
 import guitests.guihandles.SourceListPanelHandle;
-import guitests.guihandles.ResultDisplayHandle;
 import seedu.address.model.source.Source;
 
 /**

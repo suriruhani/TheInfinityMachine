418ce7da362ec556b96845f2dd8a7f9e8c4d27eb
DarrenDragonLee <darren.dragon@live.com.sg>
2019-03-26 22:22:59 +0800

    More checkstyle issues

--- src/main/java/seedu/address/logic/parser/CustomOrderCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CustomOrderCommandParser.java
@@ -2,12 +2,12 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+import java.util.logging.Logger;
+
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.LogicManager;
 import seedu.address.logic.commands.CustomOrderCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-
-import java.util.logging.Logger;
 
 /**
  * Parser for the CustomOrder command which takes in a user input.

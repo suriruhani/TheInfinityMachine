65528f4355b1714f95c0a6e85a4d4cd9b3367a58
suriruhani <suriruhani@gmail.com>
2019-03-26 22:08:05 +0800

    Add ListCommandParser.java to accept user input

---
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class ListCommandParser implements Parser<ListCommand> {
+
+    public ListCommand parse(String args) throws ParseException {
+
+
+    }
+}
--- src/main/java/seedu/address/logic/parser/SourceManagerParser.java
+++ src/main/java/seedu/address/logic/parser/SourceManagerParser.java
@@ -109,7 +109,7 @@ public class SourceManagerParser implements CommandValidator {
             return new SearchCommandParser().parse(arguments);
 
         case ListCommand.COMMAND_WORD:
-            return new ListCommand();
+            return new ListCommandParser().parse(arguments);
 
         case HistoryCommand.COMMAND_WORD:
             return new HistoryCommand();

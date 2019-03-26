c0dda86de022ada49ee3fb2cdb741df073a0f7e0
DoItTomorrow <E0177248@u.nus.edu>
2019-03-26 21:08:58 +0800

    Added support for calling biblio command Added BiblioCommandParser to Parser module Added reference to BiblioCommandParser in SourceManagerParser Basic functionality implemented for BiblioCommand

--- src/main/java/seedu/address/logic/commands/BiblioCommand.java
+++ src/main/java/seedu/address/logic/commands/BiblioCommand.java
@@ -1,25 +1,51 @@
 package seedu.address.logic.commands;
 
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.source.Source;
 
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
 
 /**
- * Generated a bibliography of the appropriate style.
+ * Generates a bibliography from a source at the specified index.
  */
 
 public class BiblioCommand extends Command {
     public static final String COMMAND_WORD = "biblio";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Generates a bibliography in appropriate style";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Generates a bibliography in appropriate style\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_SUCCESS = "Bibliography generated";
 
-    public BiblioCommand() {}
-
-    @Override
-    public CommandResult execute(Model model, CommandHistory history) {
-        return new CommandResult(MESSAGE_SUCCESS);
+    public BiblioCommand(Index targetIndex){
+        this.targetIndex = targetIndex;
     }
 
+    private final Index targetIndex;
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+        List<Source> lastShownList = model.getFilteredSourceList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_SOURCE_DISPLAYED_INDEX);
+        }
+
+        Source targetSource = lastShownList.get(targetIndex.getZeroBased());
+
+        String targetTitle = targetSource.getTitle().title;
+        String targetType = targetSource.getType().type;
+        String targetDetail = targetSource.getDetail().detail;
+
+        return new CommandResult(String.format("%s;\n%s;\n%s\n", targetTitle, targetType, targetDetail));
+    }
 }
--- src/main/java/seedu/address/logic/parser/SourceManagerParser.java
+++ src/main/java/seedu/address/logic/parser/SourceManagerParser.java
@@ -6,23 +6,7 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.CountCommand;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.GreetCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.HistoryCommand;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.PanicCommand;
-import seedu.address.logic.commands.RedoCommand;
-import seedu.address.logic.commands.SearchCommand;
-import seedu.address.logic.commands.SelectCommand;
-import seedu.address.logic.commands.UndoCommand;
-import seedu.address.logic.commands.UnpanicCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -105,6 +89,9 @@ public class SourceManagerParser {
 
         case GreetCommand.COMMAND_WORD:
             return new GreetCommand();
+
+        case BiblioCommand.COMMAND_WORD:
+            return new BiblioCommandParser().parse(arguments);
 
         default:
             // Check if input is an alias

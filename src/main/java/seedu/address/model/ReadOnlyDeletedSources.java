eae577a45abb89d23af3f85ceb3242ec46294df4
Cassey Low <casseyl141@gmail.com>
2019-03-31 16:19:28 +0800

    add ReadOnlyDeletedSources interface

---
+++ src/main/java/seedu/address/model/ReadOnlyDeletedSources.java
@@ -0,0 +1,16 @@
+package seedu.address.model;
+
+import seedu.address.commons.core.GuiSettings;
+
+import java.nio.file.Path;
+
+/**
+ * Unmodifiable view of recently deleted.
+ */
+public interface ReadOnlyDeletedSources {
+
+    GuiSettings getGuiSettings();
+
+    Path getDeletedSourceFilePath();
+
+}

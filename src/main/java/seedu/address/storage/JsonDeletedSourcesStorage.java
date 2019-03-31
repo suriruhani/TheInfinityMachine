754fbaa29206420c8d2ef28c8d2814f6554b5bef
Cassey Low <casseyl141@gmail.com>
2019-03-31 17:30:08 +0800

    add DeletedSource functionality to JsonDeletedSourcesStorage

--- src/main/java/seedu/address/storage/JsonDeletedSourcesStorage.java
+++ src/main/java/seedu/address/storage/JsonDeletedSourcesStorage.java
@@ -1,4 +1,45 @@
 package seedu.address.storage;
 
-public class JsonDeletedSourcesStorage {
+import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.commons.util.JsonUtil;
+import seedu.address.model.DeletedSources;
+import seedu.address.model.ReadOnlyDeletedSources;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+public class JsonDeletedSourcesStorage implements DeletedSourcesStorage {
+
+
+    private Path filePath;
+
+    public JsonDeletedSourcesStorage(Path filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public Path getDeletedSourceFilePath() {
+        return filePath;
+    }
+
+    @Override
+    public Optional<DeletedSources> readDeletedSources() throws DataConversionException {
+        return readDeletedSources(filePath);
+    }
+
+    /**
+     * Similar to {@link #readDeletedSources()}
+     * @param prefsFilePath location of the data. Cannot be null.
+     * @throws DataConversionException if the file format is not as expected.
+     */
+    public Optional<DeletedSources> readDeletedSources(Path prefsFilePath) throws DataConversionException {
+        return JsonUtil.readJsonFile(prefsFilePath, DeletedSources.class);
+    }
+
+    @Override
+    public void saveDeletedSources(ReadOnlyDeletedSources deletedSources) throws IOException {
+        JsonUtil.saveJsonFile(deletedSources, filePath);
+    }
+
 }

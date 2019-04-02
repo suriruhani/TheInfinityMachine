a988c3779d1b72283d1856e8bf9b680641088fd1
suriruhani <suriruhani@gmail.com>
2019-04-02 20:27:07 +0800

    modify SourceContainsKeywordsPredicate to split parameters into title, detail, type, source and tags

--- src/main/java/seedu/address/model/source/SourceContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/source/SourceContainsKeywordsPredicate.java
@@ -1,9 +1,15 @@
 package seedu.address.model.source;
 
+import java.util.List;
+import java.util.Set;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ParserUtil;
+import seedu.address.model.tag.Tag;
+
+import static seedu.address.logic.parser.CliSyntax.*;
 
 /**
  * Tests that a {@code Source}'s {@code Title}, {@code Type}, {@code Source} and {@code Detail}
@@ -19,9 +25,56 @@ public class SourceContainsKeywordsPredicate implements Predicate<Source> {
 
     @Override
     public boolean test(Source source) {
-        return true;
+        boolean result = true;
+
+        String titleKeywords = keywords.getValue(PREFIX_TITLE).get();
+        String typeKeywords = keywords.getValue(PREFIX_TYPE).get();
+        String detailsKeywords = keywords.getValue(PREFIX_DETAILS).get();
+        List<String> tagKeywords = keywords.getAllValues(PREFIX_TAG);
+
+        if (titleKeywords.equals("") && tagKeywords.isEmpty() && typeKeywords.equals("")
+                && detailsKeywords.equals("")) {
+            return false;
+        }
+
+        if (!titleKeywords.equals("")) {
+            result = result && matchTitleKeywords(titleKeywords, source);
+        } 
+        
+        if (!typeKeywords.equals("")) {
+            result = result && matchTypeKeywords(typeKeywords, source);
+        }
+        
+        if (!detailsKeywords.equals("")) {
+            result = result && matchDetailKeywords(detailsKeywords, source);
+        }
+        
+        if (!tagKeywords.isEmpty()) {
+            result = result && matchTagKeywords(tagKeywords, source);
+        }
+
+        return result;
+
         // return keywords.stream()
         // .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(source.getTitle().title, keyword));
+    }
+
+    private boolean matchTagKeywords(List<String> tagKeywords, Source source) {
+
+
+        return true;
+    }
+
+    private boolean matchDetailKeywords(String detailsKeywords, Source source) {
+        return true;
+    }
+
+    private boolean matchTypeKeywords(String typeKeywords, Source source) {
+        return true;
+    }
+
+    private boolean matchTitleKeywords(String titleKeywords, Source source) {
+        return true;
     }
 
     @Override

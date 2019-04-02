ae04a2116204c6806571211e919247e4b3fbc665
DoItTomorrow <43004683+DoItTomorrow@users.noreply.github.com>
2019-04-03 01:09:21 +0800

    Update BiblioCommandTest.java
    Import getTypicalDeletedSources

--- src/test/java/seedu/address/logic/commands/BiblioCommandTest.java
+++ src/test/java/seedu/address/logic/commands/BiblioCommandTest.java
@@ -4,7 +4,7 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_SOURCE;
 import static seedu.address.testutil.TypicalSources.getTypicalSourceManager;
-import static seedu.address.testutil.TypicalSources.getTypicalSourceManager;
+import static seedu.address.testutil.TypicalSources.getTypicalDeletedSources;
 
 import org.junit.Test;
 

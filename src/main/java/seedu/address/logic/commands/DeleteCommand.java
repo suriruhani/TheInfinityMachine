721331fda7def4d37793d21d525c54e285e98420
Cassey Low <casseyl141@gmail.com>
2019-04-06 23:13:28 +0800

    update delete command javadocs

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -12,7 +12,7 @@ import seedu.address.model.Model;
 import seedu.address.model.source.Source;
 
 /**
- * Deletes a source identified using it's displayed index from the address book.
+ * Deletes a source identified using it's displayed index from the source manager.
  */
 public class DeleteCommand extends Command {
 

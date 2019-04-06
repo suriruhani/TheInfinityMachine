123f71b82f8cca90473387281c2f5ee9b473279e
Cassey Low <casseyl141@gmail.com>
2019-04-06 17:02:28 +0800

    change success message for source details

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -23,8 +23,8 @@ public class DeleteCommand extends Command {
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_DELETE_SOURCE_SUCCESS = "Deleted Source: %1$s";
-
+    public static final String MESSAGE_DELETE_SOURCE_SUCCESS = "Deleted Source:\n---------------------" +
+            "--------------\n%1$s";
     private final Index targetIndex;
 
     public DeleteCommand(Index targetIndex) {

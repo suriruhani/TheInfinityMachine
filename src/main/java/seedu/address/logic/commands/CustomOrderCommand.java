7acb5eb8d3852f8f97855975c81defd799584791
DarrenDragonLee <darren.dragon@live.com.sg>
2019-04-10 00:05:41 +0800

    Minor rearrangement of imports

--- src/main/java/seedu/address/logic/commands/CustomOrderCommand.java
+++ src/main/java/seedu/address/logic/commands/CustomOrderCommand.java
@@ -9,8 +9,8 @@ import java.util.logging.Logger;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.LogicManager;
-import seedu.address.model.PinnedSourcesCoordinationCenter;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.PinnedSourcesCoordinationCenter;
 import seedu.address.model.Model;
 import seedu.address.model.source.Source;
 

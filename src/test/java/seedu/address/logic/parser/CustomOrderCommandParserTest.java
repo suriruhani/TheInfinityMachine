24e594943bdc3c4a13501716f428607b74a8a2e2
DarrenDragonLee <darren.dragon@live.com.sg>
2019-04-10 00:22:43 +0800

    Even more rearrangements

--- src/test/java/seedu/address/logic/parser/CustomOrderCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/CustomOrderCommandParserTest.java
@@ -1,7 +1,8 @@
 package seedu.address.logic.parser;
 
-import org.junit.Test;
 import seedu.address.logic.commands.CustomOrderCommand;
+
+import org.junit.Test;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;

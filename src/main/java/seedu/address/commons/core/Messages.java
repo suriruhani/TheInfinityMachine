9b1397fd3c10d695b82a8fbd11f1b26d047e945c
Cassey Low <casseyl141@gmail.com>
2019-04-11 22:11:56 +0800

    remove unused message

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -8,8 +8,6 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command.";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_SOURCE_DISPLAYED_INDEX = "The source index provided is invalid";
-    public static final String MESSAGE_UNAVAILABLE_COMMAND = "This command is unavailable in this mode,\n"
-            + " switch to the Recycle Bin mode to use it.";
     public static final String MESSAGE_DUPLICATE_SOURCE_TO_RESTORE = "The source you are trying to restore already "
             + "exists in the current source list.\nThe duplicate source will be removed from your deleted source list.";
     public static final String MESSAGE_SOURCES_LISTED_OVERVIEW = "%1$d sources listed!";

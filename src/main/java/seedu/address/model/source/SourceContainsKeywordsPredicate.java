26e173163c931ee31f9aad486dc509899c8b86b5
suriruhani <suriruhani@gmail.com>
2019-04-11 23:00:24 +0800

    updates javadoc

--- src/main/java/seedu/address/model/source/SourceContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/source/SourceContainsKeywordsPredicate.java
@@ -72,9 +72,9 @@ public class SourceContainsKeywordsPredicate implements Predicate<Source> {
 
     /**
      * Evaluates the number of swaps needed to transform one string to the other [case insensitive]
-     * @param a
-     * @param b
-     * @return int number of swaps needed
+     * @param a a string to be transformed from
+     * @param b a string to be transformed to
+     * @return int number of swaps needed for the transformation
      */
     public static int levenshtienDist(String a, String b) {
         int [] costs = new int [b.length() + 1];
@@ -82,7 +82,6 @@ public class SourceContainsKeywordsPredicate implements Predicate<Source> {
             costs[j] = j;
         }
         for (int i = 1; i <= a.length(); i++) {
-            // j == 0; nw = lev(i - 1, j)
             costs[0] = i;
             int nw = i - 1;
             for (int j = 1; j <= b.length(); j++) {

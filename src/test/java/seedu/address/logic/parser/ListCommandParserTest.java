b25fd3c621150377554adf17a00f68ecbc6b2f45
suriruhani <suriruhani@gmail.com>
2019-04-13 21:51:50 +0800

    fix listcommandparser tests

--- src/test/java/seedu/address/logic/parser/ListCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ListCommandParserTest.java
@@ -1,22 +1,11 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.commands.CommandTestUtil.TAG_DESC_BAR;
-import static seedu.address.logic.commands.CommandTestUtil.TITLE_DESC_ENGINEERING;
-import static seedu.address.logic.commands.CommandTestUtil.TYPE_DESC_ENGINEERING;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DETAILS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TITLE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TYPE;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.SearchCommand;
-import seedu.address.model.source.SourceContainsKeywordsPredicate;
 
 public class ListCommandParserTest {
 
@@ -51,39 +40,10 @@ public class ListCommandParserTest {
     }
 
     @Test
-    public void parse_twoNonRangeArgs_returnsSearchCommand() {
-        ListCommand expectedListCommand = new ListCommand(Index.fromOneBased(3), Index.fromOneBased(2));
-        assertParseFailure(parser, " 3 2" , ListCommand.MESSAGE_USAGE);
+    public void parse_twoSameArgs_returnsSearchCommand() {
+        ListCommand expectedListCommand = new ListCommand(Index.fromOneBased(2), Index.fromOneBased(2));
+        assertParseSuccess(parser, " 2 2" , expectedListCommand);
     }
 
-    @Test
-    public void parse_validArgs_returnsSearchCommand() {
-        String input = TITLE_DESC_ENGINEERING;
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(input,
-                PREFIX_TITLE, PREFIX_TYPE, PREFIX_DETAILS, PREFIX_TAG);
-        // no leading and trailing whitespaces
-        SearchCommand expectedSearchCommand = new SearchCommand(new SourceContainsKeywordsPredicate(argMultimap));
-        assertParseSuccess(parser, TITLE_DESC_ENGINEERING, expectedSearchCommand);
-        // multiple whitespaces between keywords
-        assertParseSuccess(parser, "\n \t" + TITLE_DESC_ENGINEERING, expectedSearchCommand);
-    }
-
-    @Test
-    public void parse_validMultipleArgs_returnsSearchCommand() {
-        String input = TYPE_DESC_ENGINEERING + TYPE_DESC_ENGINEERING;
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(input,
-                PREFIX_TITLE, PREFIX_TYPE, PREFIX_DETAILS, PREFIX_TAG);
-        SearchCommand expectedSearchCommand = new SearchCommand(new SourceContainsKeywordsPredicate(argMultimap));
-        assertParseSuccess(parser, TYPE_DESC_ENGINEERING + TYPE_DESC_ENGINEERING, expectedSearchCommand);
-    }
-
-    @Test
-    public void parse_validCompoundArgs_returnsSearchCommand() {
-        String input = TYPE_DESC_ENGINEERING + TAG_DESC_BAR;
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(input,
-                PREFIX_TITLE, PREFIX_TYPE, PREFIX_DETAILS, PREFIX_TAG);
-        SearchCommand expectedSearchCommand = new SearchCommand(new SourceContainsKeywordsPredicate(argMultimap));
-        assertParseSuccess(parser, TYPE_DESC_ENGINEERING + TAG_DESC_BAR, expectedSearchCommand);
-    }
 
 }

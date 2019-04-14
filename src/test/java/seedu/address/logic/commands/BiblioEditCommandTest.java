db06a51620af73dbc277898a02a413fa6e27cd66
DoItTomorrow <E0177248@u.nus.edu>
2019-04-15 00:45:17 +0800

    Checkstyle Edits

--- src/test/java/seedu/address/logic/commands/BiblioEditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/BiblioEditCommandTest.java
@@ -1,7 +1,7 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_SOURCE;
 import static seedu.address.testutil.TypicalSources.getTypicalDeletedSources;
 import static seedu.address.testutil.TypicalSources.getTypicalSourceManager;

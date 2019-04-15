7042b597407b9cc0600b164f3a078c5d25a778d8
DoItTomorrow <E0177248@u.nus.edu>
2019-04-15 13:15:30 +0800

    Removed unnecessay import in util/TypicalSources.java

--- src/main/java/seedu/address/model/util/TypicalSources.java
+++ src/main/java/seedu/address/model/util/TypicalSources.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import seedu.address.model.DeletedSources;
-import seedu.address.model.SourceManager;
 import seedu.address.model.source.Source;
 
 /**
